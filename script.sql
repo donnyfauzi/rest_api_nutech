971b0d6c44b107660cb8d46d6c3ca231ea954b1c
donnyfauzi <donnyfauzi55@gmail.com>
2024-11-23 16:18:00 +0700

    update ddl

--- script.sql
+++ script.sql
@@ -1,73 +1,210 @@
--- Database :nutech_db
-CREATE DATABASE nutech_db;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Nov 2024 pada 07.56
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
--- Tabel Users
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    balance DECIMAL(10, 0) DEFAULT 0 NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    profile_image VARCHAR(255) DEFAULT NULL
-);
-
--- Table Banner
-CREATE TABLE banners (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    banner_name VARCHAR(255) NOT NULL,
-    banner_image VARCHAR(255) NOT NULL,
-    description TEXT
-);
-
--- Values
-INSERT INTO banners (banner_name, banner_image, description) VALUES
-('Banner 1', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
-('Banner 2', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
-('Banner 3', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
-('Banner 4', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
-('Banner 5', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
-('Banner 6', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet');
-
--- Table Services
-CREATE TABLE services (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    service_code VARCHAR(50) NOT NULL UNIQUE,
-    service_name VARCHAR(255) NOT NULL,
-    service_icon VARCHAR(255) NOT NULL,
-    service_tariff DECIMAL(10, 0) NOT NULL
-);
-
--- values
-INSERT INTO services (service_code, service_name, service_icon, service_tariff)
-VALUES 
-    ('PAJAK', 'Pajak PBB', 'https://nutech-integrasi.app/dummy.jpg', 40000),
-    ('PLN', 'Listrik', 'https://nutech-integrasi.app/dummy.jpg', 10000),
-    ('PDAM', 'PDAM Berlangganan', 'https://nutech-integrasi.app/dummy.jpg', 40000),
-    ('PULSA', 'Pulsa', 'https://nutech-integrasi.app/dummy.jpg', 40000),
-    ('PGN', 'PGN Berlangganan', 'https://nutech-integrasi.app/dummy.jpg', 50000),
-    ('MUSIK', 'Musik Berlangganan', 'https://nutech-integrasi.app/dummy.jpg', 50000),
-    ('TV', 'TV Berlangganan', 'https://nutech-integrasi.app/dummy.jpg', 50000),
-    ('PAKET_DATA', 'Paket Data', 'https://nutech-integrasi.app/dummy.jpg', 50000),
-    ('VOUCHER_GAME', 'Voucher Game', 'https://nutech-integrasi.app/dummy.jpg', 100000),
-    ('VOUCHER_MAKANAN', 'Voucher Makanan', 'https://nutech-integrasi.app/dummy.jpg', 100000),
-    ('QURBAN', 'Qurban', 'https://nutech-integrasi.app/dummy.jpg', 200000),
-    ('ZAKAT', 'Zakat', 'https://nutech-integrasi.app/dummy.jpg', 300000);
-
--- Tabel Transaksi 
-CREATE TABLE transactions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,  
-    service_code VARCHAR(50) NOT NULL,  
-    service_name VARCHAR(255) NOT NULL,  
-    transaction_type VARCHAR(50) NOT NULL,  
-    total_amount DECIMAL(10, 0) NOT NULL,  
-    invoice_number VARCHAR(50) UNIQUE NOT NULL,  
-    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,  
-    FOREIGN KEY (service_code) REFERENCES services(service_code) ON DELETE CASCADE  
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nutech_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `banners`
+--
+
+CREATE TABLE `banners` (
+  `id` int(11) NOT NULL,
+  `banner_name` varchar(255) NOT NULL,
+  `banner_image` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `banners`
+--
+
+INSERT INTO `banners` (`id`, `banner_name`, `banner_image`, `description`) VALUES
+(1, 'Banner 1', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
+(2, 'Banner 2', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
+(3, 'Banner 3', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
+(4, 'Banner 4', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
+(5, 'Banner 5', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet'),
+(6, 'Banner 6', 'https://nutech-integrasi.app/dummy.jpg', 'Lerem Ipsum Dolor sit amet');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `service_code` varchar(50) NOT NULL,
+  `service_name` varchar(255) NOT NULL,
+  `service_icon` varchar(255) NOT NULL,
+  `service_tariff` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `services`
+--
+
+INSERT INTO `services` (`id`, `service_code`, `service_name`, `service_icon`, `service_tariff`) VALUES
+(1, 'PAJAK', 'Pajak PBB', 'https://nutech-integrasi.app/dummy.jpg', 40000),
+(2, 'PLN', 'Listrik', 'https://nutech-integrasi.app/dummy.jpg', 10000),
+(3, 'PDAM', 'PDAM Berlangganan', 'https://nutech-integrasi.app/dummy.jpg', 40000),
+(4, 'PULSA', 'Pulsa', 'https://nutech-integrasi.app/dummy.jpg', 40000),
+(5, 'PGN', 'PGN Berlangganan', 'https://nutech-integrasi.app/dummy.jpg', 50000),
+(6, 'MUSIK', 'Musik Berlangganan', 'https://nutech-integrasi.app/dummy.jpg', 50000),
+(7, 'TV', 'TV Berlangganan', 'https://nutech-integrasi.app/dummy.jpg', 50000),
+(8, 'PAKET_DATA', 'Paket Data', 'https://nutech-integrasi.app/dummy.jpg', 50000),
+(9, 'VOUCHER_GAME', 'Voucher Game', 'https://nutech-integrasi.app/dummy.jpg', 100000),
+(10, 'VOUCHER_MAKANAN', 'Voucher Makanan', 'https://nutech-integrasi.app/dummy.jpg', 100000),
+(11, 'QURBAN', 'Qurban', 'https://nutech-integrasi.app/dummy.jpg', 200000),
+(12, 'ZAKAT', 'Zakat', 'https://nutech-integrasi.app/dummy.jpg', 300000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `invoice_number` varchar(255) NOT NULL,
+  `service_code` varchar(50) NOT NULL,
+  `transaction_type` enum('PAYMENT','REFUND') NOT NULL,
+  `total_amount` decimal(15,2) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `service_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `user_id`, `invoice_number`, `service_code`, `transaction_type`, `total_amount`, `created_on`, `updated_on`, `service_name`) VALUES
+(7, 1, 'INV241122-162', 'PLN', 'PAYMENT', 10000.00, '2024-11-22 13:32:34', '2024-11-22 13:32:34', 'Listrik Prabayar'),
+(9, 1, 'INV241122-235', 'PAKET_DATA', 'PAYMENT', 50000.00, '2024-11-22 13:34:42', '2024-11-22 13:34:42', 'Paket Data'),
+(10, 1, 'INV241122-867', 'MUSIK', 'PAYMENT', 50000.00, '2024-11-22 13:35:27', '2024-11-22 13:35:27', 'Musik Berlangganan'),
+(11, 1, 'INV241122-702', 'PDAM', 'PAYMENT', 40000.00, '2024-11-22 13:36:02', '2024-11-22 13:36:02', 'PDAM Berlangganan'),
+(12, 1, 'INV241122-982', 'TV', 'PAYMENT', 50000.00, '2024-11-22 13:37:08', '2024-11-22 13:37:08', 'TV Berlangganan'),
+(13, 1, 'INV241123-543', 'ZAKAT', 'PAYMENT', 300000.00, '2024-11-23 02:50:08', '2024-11-23 02:50:08', 'Zakat');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `profile_image` varchar(255) DEFAULT NULL,
+  `balance` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `first_name`, `last_name`, `created_at`, `updated_at`, `profile_image`, `balance`) VALUES
+(1, 'user@nutech-integrasi.com', '$2a$10$aIixZFJrUYGdXV1rlHPXWefpuHX/70.E/7Q2G1vS0m2NB440yblPq', 'Donny', 'Fauzi', '2024-11-21 15:44:15', '2024-11-23 02:50:08', NULL, 1900000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `service_code` (`service_code`);
+
+--
+-- Indeks untuk tabel `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_id` (`user_id`),
+  ADD KEY `fk_service_code` (`service_code`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `banners`
+--
+ALTER TABLE `banners`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `fk_service_code` FOREIGN KEY (`service_code`) REFERENCES `services` (`service_code`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
